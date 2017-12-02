fb44ac49672852e0f30ea4de9a1e96b968d3a971
popoparo <stas.hazhevskiy@gmail.com>
2017-12-02 13:26:16 +0200

    added Task5

--- Module/Task5/Task5.java
+++ Module/Task5/Task5.java
@@ -1,27 +1,40 @@
 package Module.Task5;
 
-import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
-
 import java.util.Scanner;
 
 public class Task5 {
-    public static void main(String[] args) {
-        int x = 8;
-        int y = 8;
+    public static void main(String[] args) throws Exception {
 
-        Scanner scannerX = new Scanner(System.in);
-        Scanner scannerY = new Scanner(System.in);
-        Scanner scannerXhod = new Scanner(System.in);
-        Scanner scannerYhod = new Scanner(System.in);
-        System.out.println("Введите координату Х начала");
-        int coordinateX = scannerX.nextInt();
-        System.out.println("Введите координату Y начала");
-        int coordinateY = scannerY.nextInt();
+        Scanner scannerStart = new Scanner(System.in);
+        Scanner scannerStep = new Scanner(System.in);
 
-        System.out.println("Введите Х хода");
-        int coordXhod = scannerXhod.nextInt();
-        System.out.println("Введите Y хода");
-        int coordYhod = scannerYhod.nextInt();
+        System.out.println("Введите координату Х начала(От 0 до 8)");
+        int xStart = scannerStart.nextInt();
+        System.out.println("Введите координату Y начала(От 0 до 8)");
+        int yStart = scannerStart.nextInt();
+        while (true) {
+            System.out.println("Введите Х хода");
+            int xStep = scannerStep.nextInt();
+            System.out.println("Введите Y хода");
+            int yStep = scannerStep.nextInt();
+            if (
+                    (xStep == xStart - 2 && yStep == yStart - 1) ||
+                            (xStep == xStart - 2 && yStep == yStart + 1) ||
+                            (xStep == xStart + 2 && yStep == yStart - 1) ||
+                            (xStep == xStart + 2 && yStep == yStart + 1) ||
+                            (yStep == yStart - 2 && xStep == xStart - 1) ||
+                            (yStep == yStart - 2 && xStep == xStart + 1) ||
+                            (yStep == yStart + 2 && xStep == xStart - 1) ||
+                            (yStep == yStart + 2 && xStep == xStart + 1)
 
+                                    && xStep <= 8 && xStep >= 0
+                                    && yStep <= 8 && yStep >= 0)
+
+            {
+                System.out.println("Вы походили на: " + "х: " + xStep + " y: " + yStep);
+            } else {
+                System.out.println("Неправилный ход");
+            }
+        }
     }
 }
